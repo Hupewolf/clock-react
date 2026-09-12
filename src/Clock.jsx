a599f361a5562160e08d4f19a7920cfd17bd5575
Hupewolf <nhannguyen.ix5@gmail.com>
2026-09-12 22:31:59 +0700

    test

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -1,48 +1,63 @@
 import { Sun, Bell, Moon } from 'lucide-react';
 import { Thermometer } from "./components/thermometer";
-
+import { useState, useEffect } from "react";
 
 function Clock() {
-    let thang = new Date().toLocaleDateString('en-US', {month: 'short'}).toUpperCase();
-    let ngay = new Date().getDate();
-    let thu = new Date().toLocaleDateString('en-US', {weekday: 'short'}).toUpperCase();
-    let gio = new Date().getHours();
-    let phut = new Date().getMinutes();
+
+    const [now, setNow] = useState(new Date());
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setNow(new Date());
+        }, 1000);
+        return () => {
+            clearInterval(timer);
+        };
+    }, []);
+    let thang = now.toLocaleDateString('en-US', { month: 'short' }).toUpperCase();
+    let ngay = now.getDate();
+    let thu = now.toLocaleDateString('en-US', { weekday: 'short' }).toUpperCase();
+    let gio = now.getHours();
+    let phut = now.getMinutes();
+    let giay = now.getSeconds();
 
     gio = gio < 10 ? `0${gio}` : gio;
     phut = phut < 10 ? `0${phut}` : phut;
 
     let isDay;
 
-    if ( gio >= 6 && gio <= 17) {
+    if (gio >= 6 && gio <= 17) {
         isDay = true;
-        document.documentElement.style.setProperty('--text-color', '#fffdae');
+        document.documentElement.style.setProperty('--text-color', '#FEFDAB');
         document.documentElement.style.setProperty('--shadow-text-color', 'rgba(221, 63, 15,0.8)');
     }
     else {
         isDay = false;
-        document.documentElement.style.setProperty('--text-color', '#8bf7ff');
+        document.documentElement.style.setProperty('--text-color', '#7AF7FD');
         document.documentElement.style.setProperty('--shadow-text-color', 'rgba(0,110,255,0.8)');
     }
 
     let buoi;
 
-    if ( gio >= 5 && gio < 12 ) {
+    if (gio >= 5 && gio < 12) {
         buoi = "Good Morning";
     }
-    else if ( gio >= 12 && gio < 18 ) {
+    else if (gio >= 12 && gio < 18) {
         buoi = "Good Afternoon";
     }
     else {
         buoi = "Good Evening";
     }
-    
+
+    const hourAngle = (gio % 12) * 30 + phut * 0.5 - 90;
+    const minuteAngle = phut * 6 + giay * 0.1 - 90;
+    const secondAngle = giay * 6 - 90;
+
 
     return (
         <div className="outer-clock">
             <div className="clock">
                 <div className="left-left">
-                    <Bell/>
+                    <Bell />
                     <div>
                         {isDay ? <Sun /> : <Moon />}
                     </div>
@@ -53,6 +68,37 @@ function Clock() {
                     <div id="phut">{phut}</div>
                 </div>
                 <div className="line-row"></div>
+                <div className="clock-circle">
+                    <div class="marker marker-1"><div class="marker-dot"></div></div>
+                    <div class="marker marker-2"><div class="marker-dot"></div></div>
+                    <div class="marker marker-3"><div class="marker-dot"></div></div>
+                    <div class="marker marker-4"><div class="marker-dot"></div></div>
+                    <div class="marker marker-5"><div class="marker-dot"></div></div>
+                    <div class="marker marker-6"><div class="marker-dot"></div></div>
+                    <div class="marker marker-7"><div class="marker-dot"></div></div>
+                    <div class="marker marker-8"><div class="marker-dot"></div></div>
+                    <div class="marker marker-9"><div class="marker-dot"></div></div>
+                    <div class="marker marker-10"><div class="marker-dot"></div></div>
+                    <div class="marker marker-11"><div class="marker-dot"></div></div>
+                    <div class="marker marker-12"><div class="marker-dot"></div></div>
+
+                    <div class="number number-1"><span class="number-1-text">1</span></div>
+                    <div class="number number-2"><span class="number-2-text">2</span></div>
+                    <div class="number number-3"><span class="number-3-text">3</span></div>
+                    <div class="number number-4"><span class="number-4-text">4</span></div>
+                    <div class="number number-5"><span class="number-5-text">5</span></div>
+                    <div class="number number-6"><span class="number-6-text">6</span></div>
+                    <div class="number number-7"><span class="number-7-text">7</span></div>
+                    <div class="number number-8"><span class="number-8-text">8</span></div>
+                    <div class="number number-9"><span class="number-9-text">9</span></div>
+                    <div class="number number-10"><span class="number-10-text">10</span></div>
+                    <div class="number number-11"><span class="number-11-text">11</span></div>
+                    <div class="number number-12"><span class="number-12-text">12</span></div>
+                    <div className="hand hour" style={{ transform: `rotate(${hourAngle}deg)` }} ></div>
+                    <div className="hand minute" style={{ transform: `rotate(${minuteAngle}deg)` }} ></div>
+                    <div className="hand second" style={{ transform: `rotate(${secondAngle}deg)` }} ></div>
+                    <div className="center"></div>
+                </div>
                 <div className="right">
                     <div className="right-top">
                         <div id="thu">{thu}</div>
